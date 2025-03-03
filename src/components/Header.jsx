d6265e01842786b342b44961daae65bb613d4255
CedricA <cedric.j.aoun@gmail.com>
2025-03-03 14:07:19 +0100

    review changes + added Footer component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
       <img src='https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png' alt='site logo' />
       <nav>
         <ul>
-          <li><a href='#'>Home</a></li>
+          <li><a href='/'>Home</a></li>
         </ul>
       </nav>
     </>
