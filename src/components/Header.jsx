63df1a296975aee51266c4b7f7abaa980f05bf67
CedricA <cedric.j.aoun@gmail.com>
2025-03-03 13:57:41 +0100

    boilerplate cleanup + added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+      <img src='https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png' alt='site logo' />
+      <nav>
+        <ul>
+          <li><a href='#'>Home</a></li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Header
