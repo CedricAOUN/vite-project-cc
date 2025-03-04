db9493084233f315499d4e8891909fdf93f8e7d1
CedricA <cedric.j.aoun@gmail.com>
2025-03-04 14:24:49 +0100

    added conditional 'new' badge to dishes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <main>
         <Container>
           <Row>
-            <Col md={3} lg><Dish name='Tacos à l’unité' price={3} imgUrl='https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg' /></Col>
+            <Col md={3} lg><Dish isNew name='Tacos à l’unité' price={3} imgUrl='https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg' /></Col>
             <Col md={3} lg><Dish name='Enchiladas' price={12} imgUrl='https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg' /></Col>
             <Col md={3} lg><Dish name='Mole poblano' price={15} imgUrl='https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg' /></Col>
           </Row>
--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,10 +1,11 @@
-import { Card } from "react-bootstrap"
+import { Card, Badge } from "react-bootstrap"
 import './dish.scss'
 
-function Dish({ name, imgUrl, price }) {
+function Dish({ name, imgUrl, price, isNew }) {
   return (
     <Card>
       <Card.Img src={imgUrl} alt={`Image de ${name}`} />
+      {isNew && <Badge bg='primary' className='position-absolute top-0 end-0 m-2'>Nouveau</Badge>}
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Text>
