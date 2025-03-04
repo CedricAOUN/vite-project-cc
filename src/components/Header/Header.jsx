29ef1309e5b13734b34b2ab6dce3181e7d4d2eb9
CedricA <cedric.j.aoun@gmail.com>
2025-03-04 11:31:51 +0100

    use container on navbar, add dropdown toggle

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,21 @@
-import { Nav, Navbar } from "react-bootstrap"
+import { Nav, Navbar, Container } from "react-bootstrap"
 import './header.scss'
 
 function Header() {
   return (
     <>
-      <header>
-        <Navbar expand="lg">
-          <Navbar.Brand href="/"><img src='https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png' alt='site logo' /></Navbar.Brand>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+      <header className="bg-body-tertiary">
+        <Container>
+          <Navbar expand="lg">
+            <Navbar.Brand href="/"><img src='https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png' alt='site logo' /></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
       </header>
     </>
   )
