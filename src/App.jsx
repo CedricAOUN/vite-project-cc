0d5ce31aadbd3c5661a6bfab5c87ef598439894b
CedricA <cedric.j.aoun@gmail.com>
2025-03-05 09:50:14 +0100

    13: added showNewOnly button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import './App.scss'
 import Dish from './components/Dish/Dish'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import { useState } from 'react'
 
 const DISHES = [
   { name: 'Tacos à l’unité', price: 3, imgUrl: 'https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg', isNew: true, stock: 12 },
@@ -10,14 +11,24 @@ const DISHES = [
   { name: 'Mole poblano', price: 15, imgUrl: 'https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg', stock: 5 },
 ];
 
-const filteredDishes = DISHES.filter(dish => dish.stock > 0);
+
+
 
 function App() {
+  const [showNewOnly, setShowNewOnly] = useState(false);
+
+  const filteredDishes = DISHES.filter(dish => showNewOnly ? dish.isNew === true : dish.stock > 0);
+
+  const handleShowNewOnly = () => {
+    setShowNewOnly(!showNewOnly);
+  }
+
   return (
     <>
       <Header />
       <main>
         <Container>
+          <Button variant="primary" onClick={handleShowNewOnly}>{showNewOnly ? 'Voir tous les plats' : 'Nouveautés uniquement'}</Button>
           <Row>
             {filteredDishes.map((dish, index) => {
                 return (
